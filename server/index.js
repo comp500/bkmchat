526cfad138970a177c7ede356a21206818278ce7
comp500 <comp500@users.noreply.github.com>
2017-01-14 09:42:14 +0000

    change folder and serve all files

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(80);
 
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '../client/'));
 
 var users = [];
 io.on('connection', function (socket) {
