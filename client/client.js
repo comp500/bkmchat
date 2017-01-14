6c67629b7caa1b3d8b6bdecfc0c95ec32ececf9a
comp500 <comp500@users.noreply.github.com>
2017-01-14 10:00:54 +0000

    change colours to hex values, change colour on message

--- client/client.js
+++ client/client.js
@@ -6,28 +6,29 @@ chatDiv.style.bottom = "0";
 chatDiv.style.right = "0";
 chatDiv.style.width = "5px";
 chatDiv.style.height = "5px";
-chatDiv.style.backgroundColor = "red";
+chatDiv.style.backgroundColor = "#FF3B2C";
 chatDiv.style.zIndex = "1000";
 
 var user = "test1" + new Date().getSeconds(); // because i hate you
 
 socket.on("connected", function (data) {
 	console.log("connected!");
-	chatDiv.style.backgroundColor = data.notalone ? "green" : "orange";
+	chatDiv.style.backgroundColor = data.notalone ? "#25B236" : "#FFB324";
 });
 
 socket.on("joined", function (data) {
 	console.log("i'm not alone!");
-	chatDiv.style.backgroundColor = "green";
+	chatDiv.style.backgroundColor = "#25B236";
 });
 
 socket.on("alone", function (data) {
 	console.log("i'm alone");
-	chatDiv.style.backgroundColor = "orange";
+	chatDiv.style.backgroundColor = "#FFB324";
 });
 
 socket.on("message", function (data){
 	if (data.username != user) {
+		chatDiv.style.backgroundColor = "#0263CC";
 		console.log(data.username + ": " + data.message);
 	}
 });
